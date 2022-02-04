87e1f377f2d58ecc6c7cd812fe7e871622a1089f
4512 Otter Chaos <otterchaos4512@gmail.com>
2022-02-03 17:54:31 -0800

    vendordep

--- DuggCommand/src/main/java/frc/robot/subsystems/Drivetrain.java
+++ DuggCommand/src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -6,7 +6,6 @@ package frc.robot.subsystems;
 
 import static frc.robot.Constants.DriveConstants.*;
 
-import com.ctre.phoenix.motorcontrol.FollowerType;
 import com.ctre.phoenix.motorcontrol.InvertType;
 import com.ctre.phoenix.motorcontrol.NeutralMode;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
